5d9a0a06e81b2842c5b1f1f1b10a21112937815f
Julián Salgado <srjulianc25@gmail.com>
2024-12-09 22:44:59 -0500

    Create views for users and tasks

--- backend/taskmaster/users/views.py
+++ backend/taskmaster/users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import CustomUser
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
